c455c8034fd5c65ea32f1b978987d141bd9ec62d
STUDENT Naoise Quirke <Naoise.Quirke@students.ittralee.ie>
2024-09-19 10:55:05 +0100

    Signed-off-by: STUDENT Naoise Quirke <Naoise.Quirke@students.ittralee.ie>

--- RADZ/Assets/Movement.cs
+++ RADZ/Assets/Movement.cs
@@ -13,6 +13,27 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up;
+        if (Input.GetKey(KeyCode.W)) 
+            transform.position += transform.forward * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -50 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, 50 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.E)) {
+            transform.position += Vector3.forward * Time.deltaTime;
+            transform.position += Vector3.right * Time.deltaTime;
+            }
+
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.position += Vector3.forward * Time.deltaTime;
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+
     }
 }
